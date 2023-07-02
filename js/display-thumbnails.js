220632f62fc95ddbf06466a4abee9d90d83e2646
Schtock <dmitrsuvor@gmail.com>
2023-07-02 12:12:53 +1000

    Добавил деструктуризацию в createThumbnail

--- js/display-thumbnails.js
+++ js/display-thumbnails.js
@@ -3,14 +3,14 @@ const photoThumbnailTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
-const createThumbnail = (item) => {
+const createThumbnail = ({ url, description, likes, comments }) => {
   const photoThumbnail = photoThumbnailTemplate.cloneNode(true);
   const imageTemplate = photoThumbnail.querySelector('.picture__img');
 
-  imageTemplate.src = item['url'];
-  imageTemplate.alt = item['description'];
-  photoThumbnail.querySelector('.picture__likes').textContent = item['likes'];
-  photoThumbnail.querySelector('.picture__comments').textContent = item['comments'].length;
+  imageTemplate.src = url;
+  imageTemplate.alt = description;
+  photoThumbnail.querySelector('.picture__likes').textContent = likes;
+  photoThumbnail.querySelector('.picture__comments').textContent = comments.length;
 
   return photoThumbnail;
 };
