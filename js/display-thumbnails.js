b8515606c95d48761bdbc8ebbc66ff8ce21d8f16
Schtock <dmitrsuvor@gmail.com>
2023-07-06 20:22:27 +1000

    Закончил функции для модального окна

--- js/display-thumbnails.js
+++ js/display-thumbnails.js
@@ -17,8 +17,8 @@ const createThumbnail = (description) => {
   photoThumbnail.querySelector('.picture__likes').textContent = description.likes;
   photoThumbnail.querySelector('.picture__comments').textContent = description.comments.length;
 
-  photoThumbnail.addEventListener('click', (event) => {
-    event.preventDefault();
+  photoThumbnail.addEventListener('click', (evt) => {
+    evt.preventDefault();
     displayBigPicture(description);
   });
 
